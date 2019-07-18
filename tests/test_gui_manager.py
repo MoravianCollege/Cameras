119e1958fa3e28b412d340c9a84ed8b331bf1d93
WBrandes <brandesw@moravian.edu>
2019-07-18 11:07:59 -0400

    Add tests

--- tests/test_gui_manager.py
+++ tests/test_gui_manager.py
@@ -12,13 +12,7 @@ manager.app = MagicMock()
 @patch.object(manager, "do_processing", side_effect=manager.advance_screen)
 def test_general_run_through(mock_run_camera, mock_run_running, mock_camera_update, mock_running_update, mock_processing):
 
-    # camera_update = manager.update_camera_screen
-    # with patch.object(manager, "update_camera_screen") as mock_camera_update:
-    #     def side_effect(status, frame):
-    #         return camera_update(status, frame)
-    #     mock_camera_update.side_effect = side_effect
-
-    manager.start(1.0, 1.0)
+    manager.start(0.01, 0.01)
 
     assert mock_run_camera.called
     assert mock_camera_update.called
@@ -28,6 +22,43 @@ def test_general_run_through(mock_run_camera, mock_run_running, mock_camera_upda
 
     assert mock_processing.called
     assert manager.current_screen == 3
+
+    manager.return_to_start()
+    assert manager.current_screen == 0
+    assert manager.stop_processes
+
+@patch.object(manager, "run_camera_screen")
+@patch.object(manager, "run_running_screen")
+def test_advance_method(mock_run_camera, mock_run_running):
+
+    manager.advance_screen()
+
+    assert manager.current_screen == 1
+    assert mock_run_running.called
+
+    manager.advance_screen()
+
+    assert manager.current_screen == 2
+    assert mock_run_camera.called
+
+    manager.advance_screen()
+    assert manager.current_screen == 3
+
+    manager.advance_screen()
+    assert manager.current_screen == 0
+
+    manager.advance_screen(3)
+    assert manager.current_screen == 3
+
+    manager.advance_screen(0)
+    assert manager.current_screen == 0
+
+@patch.object(manager, "run_camera_screen")
+def test_start_method(mock_run_camera):
+    manager.start(0.01, 0.01)
+
+    assert manager.current_screen == 1
+    assert mock_run_camera.called
 
     manager.return_to_start()
     assert manager.current_screen == 0
